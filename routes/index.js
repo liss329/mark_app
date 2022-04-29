24ea54a4949383ceffec9d544788317b720b37a8
Lissana <liss.glace471@gmail.com>
2022-04-29 15:57:26 +0900

    feat: トップページの追加
    ・7-2. Markdownデータ管理ツール
    ・ログイン状態の確認
    ・データベース（投稿データ）の表示

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,44 @@
-var express = require('express');
-var router = express.Router();
+const express = require("express");
+// eslint-disable-next-line new-cap
+const router = express.Router();
+const knex = require("knex")({
+  client: "sqlite3",
+  connection: {
+    filename: "mark_data.sqlite3",
+  },
+  useNullAsDefault: true,
+});
+const Bookshelf = require("bookshelf")(knex);
+Bookshelf.plugin("pagination");
+
+const Users = Bookshelf.Model.extend({
+  tableName: "users",
+});
+
+const Markdata = Bookshelf.Model.extend({
+  tableName: "markdata",
+  user: function () {
+    return this.belongsTo(Users);
+  },
+});
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  if (!req.session.login) return res.redirect("/login");
+  new Markdata()
+    .orderBy("created_at", "DESC")
+    .fetchPage({ page: 1, pageSize: 10, withRelated: ["user"] })
+    .then((collection) => {
+      const data = {
+        title: "Top",
+        content: collection.toArray(),
+        user: req.session.login,
+      };
+      res.render("index", data);
+    })
+    .catch((err) => {
+      console.error(err);
+    });
 });
 
 module.exports = router;

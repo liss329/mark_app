2b9146f6de8c42ba46d7c4014df5a69ceaa99a2c
Lissana <liss.glace471@gmail.com>
2022-04-30 17:08:04 +0900

    style: prettierの適用

--- routes/add.js
+++ routes/add.js
@@ -1,44 +1,44 @@
-const express = require('express');
+const express = require("express");
 // eslint-disable-next-line new-cap
 const router = express.Router();
 const knex = require("knex")({
-    client: "sqlite3",
-    connection: {
-        filename: "mark_data.sqlite3",
-    },
-    useNullAsDefault: true,
+  client: "sqlite3",
+  connection: {
+    filename: "mark_data.sqlite3",
+  },
+  useNullAsDefault: true,
 });
 const Bookshelf = require("bookshelf")(knex);
 
 const Users = Bookshelf.Model.extend({
-    tableName: "users",
+  tableName: "users",
 });
 
 const Markdata = Bookshelf.Model.extend({
-    tableName: "markdata",
-    hasTimestamps: true, 
-    user: function(){
-        return this.belongsTo(Users);
-    }
+  tableName: "markdata",
+  hasTimestamps: true,
+  user: function () {
+    return this.belongsTo(Users);
+  },
 });
 
 router.get("/", (req, res, next) => {
-    const data = {
-        title: "Add",
-    };
-    res.render("add", data);
+  const data = {
+    title: "Add",
+  };
+  res.render("add", data);
 });
 
 router.post("/", (req, res, next) => {
-    console.log("post acsses");
-    const data = {
-        user_id: req.session.login.id,
-        title: req.body.title,
-        content: req.body.content,
-    }
-    new Markdata(data).save().then((collection) => {
-        res.redirect("/");
-    });
+  console.log("post acsses");
+  const data = {
+    user_id: req.session.login.id,
+    title: req.body.title,
+    content: req.body.content,
+  };
+  new Markdata(data).save().then((collection) => {
+    res.redirect("/");
+  });
 });
 
 module.exports = router;

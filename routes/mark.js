d8438b47728c58e16b9ea9aef2bd38cfec77ea01
Lissana <liss.glace471@gmail.com>
2022-04-30 17:13:53 +0900

    feat: データ更新機能の追加

--- routes/mark.js
+++ routes/mark.js
@@ -13,6 +13,7 @@ const Bookshelf = require("bookshelf")(knex);
 
 const Markdata = Bookshelf.Model.extend({
   tableName: "markdata",
+  hasTimestamps: true,
 });
 
 router.get("/", (req, res, next) => {
@@ -23,7 +24,6 @@ router.get("/", (req, res, next) => {
 });
 
 router.get("/:id", (req, res, next) => {
-  console.log(JSON.stringify(req.params.id));
   Markdata.query({ where: { id: req.params.id } })
     .fetch()
     .then((model) => {
@@ -34,10 +34,35 @@ router.get("/:id", (req, res, next) => {
         source: model.attributes.content,
         id: req.params.id,
         content: markdown.toHTML(model.attributes.content),
+        updated: {},
       };
       res.render("mark", data);
     })
-    .catch((err) => {});
+    .catch((err) => {
+      console.error(err);
+    });
+});
+
+router.post("/:id", (req, res, next) => {
+  new Markdata({ id: req.params.id })
+    .save({ content: req.body.source }, { patch: true })
+    .then((model) => {
+      const data = {
+        title: "Markdown",
+        dataTitle: model.attributes.title,
+        source: model.attributes.content,
+        id: req.params.id,
+        content: markdown.toHTML(model.attributes.content),
+        updated: {
+          text: "SOURCEの更新に成功しました。",
+          time: model.attributes.update_at,
+        },
+      };
+      res.render("mark", data);
+    })
+    .catch((err) => {
+      console.error(err);
+    });
 });
 
 module.exports = router;

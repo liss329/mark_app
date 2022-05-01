b92e22627f67e2139dc7d9aaf915fc47c9aa0ed2
Lissana <liss.glace471@gmail.com>
2022-05-02 00:10:18 +0900

    feat: データの削除機能追加

--- routes/mark.js
+++ routes/mark.js
@@ -44,25 +44,34 @@ router.get("/:id", (req, res, next) => {
 });
 
 router.post("/:id", (req, res, next) => {
-  new Markdata({ id: req.params.id })
-    .save({ content: req.body.source }, { patch: true })
-    .then((model) => {
-      const data = {
-        title: "Markdown",
-        dataTitle: model.attributes.title,
-        source: model.attributes.content,
-        id: req.params.id,
-        content: markdown.toHTML(model.attributes.content),
-        updated: {
-          text: "SOURCEの更新に成功しました。",
-          time: model.attributes.update_at,
-        },
-      };
-      res.render("mark", data);
-    })
-    .catch((err) => {
-      console.error(err);
+  if (req.body.update) {
+    new Markdata({ id: req.params.id })
+      .save({ content: req.body.source }, { patch: true })
+      .then((model) => {
+        const data = {
+          title: "Markdown",
+          dataTitle: model.attributes.title,
+          source: model.attributes.content,
+          id: req.params.id,
+          content: markdown.toHTML(model.attributes.content),
+          updated: {
+            text: "SOURCEの更新に成功しました。",
+            time: model.attributes.update_at,
+          },
+        };
+        res.render("mark", data);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }
+
+  if (req.body.delete) {
+    new Markdata({ id: req.params.id }).fetch().then((model) => {
+      model.destroy();
+      res.redirect("/");
     });
+  }
 });
 
 module.exports = router;
